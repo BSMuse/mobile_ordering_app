5d42abec38c748611c2309d4a74ae007dcc74d5e
bsmuse <bsmuse@tutamail.com>
2022-10-09 11:37:59 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,37 @@
+export const menuArray = [
+    {
+        name: "Pancake",
+        ingredients: ["eggs", "pancake mix", "milk", "butter", "syrup", "blueberries"],
+        price: 7,
+        emoji: "🥞",
+        id: 0
+    },
+    {
+        name: "Bagel",
+        ingredients: ["bread", "butter", "cream cheese"],
+        price: 5,
+        emoji: "🥯",
+        id: 1
+    },
+        {
+        name: "Bacon and Eggs",
+        ingredients: ["Bacon", "eggs", "pepper"],
+        price: 6, 
+        emoji: "🥓 🍳",
+        id: 2
+    }, 
+    {
+        name: "Coffee",
+        ingredients: ["coffee", "sugar", "cream"],
+        price: 2,
+        emoji: "&#9749",
+        id: 3
+    },
+    {
+        name: "Orange Juice",
+        ingredients: ["orange", "water", "sugar"],
+        price: 3,
+        emoji: "🍊🥤",
+        id: 4
+    }
+]
---
+++ script.js
@@ -0,0 +1,4 @@
+import { menuArray } from "./data"
+const body = document.querySelector('body')
+body.innerHTML =`<p>TEST</p>`
+console.log(1)
